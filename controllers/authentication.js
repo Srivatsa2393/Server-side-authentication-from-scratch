727a069d9932fa8a9fa39cddb37d73f78520dcdf
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-31 23:21:17 +0530

    creating user records

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -10,12 +10,30 @@ exports.signup = function(req, res, next){
 
   //See if a user with given email exists
   User.findOne({ email: email }, function(err, existingUser) {
-    
-  });
+    if (err){
+      return next(err);
+    }
 
   //If a user with email does exist, return an error
+    if (existingUser){
+      return res.status(422).send({ error: 'Email is in use' });
+    }
 
   //if a user with email does not exist create and save user record
+  const user = new User({
+    email: email,
+    password: password
+  });
+
+  //to save the new user to db
+  user.save(function(err) {
+    if (err) {
+      return next(err);
+    }
 
   //Respond to request indicating the user was created
+  res.json({ success: true });
+
+    });
+  });
 }
