5c57276413706ccad2e6e6a67bde5f7c8ca00e95
Srivatsa S <srivatsashankaran@gmail.com>
2017-08-03 17:27:08 +0530

    MAking an authenticated request

--- router.js
+++ router.js
@@ -1,4 +1,8 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session : false });
 
 /*  module.exports = function(app) {
     app.get('/', function(req, res, next) {
@@ -7,5 +11,8 @@ const Authentication = require('./controllers/authentication');
 }  */
 
 module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ hi: 'there' });
+    })
     app.post('/signup', Authentication.signup);
 }
